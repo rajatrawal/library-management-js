7912fc5f2ef3d723df738ad86d7b0912a99d8be3
Rajat <rwlrajat@gmail.com>
2023-10-22 13:18:19 +0530

    first commit

---
+++ indexes6.js
@@ -0,0 +1,201 @@
+class Book {
+    constructor(name,author,type) {
+        this.name = name;
+        this.author = author;
+        this.type = type;
+    }
+}
+class Display{
+    clear() {
+        let libraryFrom = document.getElementById("libraryFrom");
+        libraryFrom.reset();
+    
+    };
+   addToStorage(book) {
+        let nameText = localStorage.getItem("nameText");
+        let authorText = localStorage.getItem("authorText");
+        let typeText = localStorage.getItem("typeText");
+        if (nameText == null) {
+            nameArray = [];
+            authorArray = [];
+            typeArray = [];
+    
+        }
+        else {
+            var nameArray = JSON.parse(nameText);
+            var authorArray = JSON.parse(authorText);
+            var typeArray = JSON.parse(typeText);
+        }
+        let bookName = book.name;
+        let authorName = book.author;
+        let bookType = book.type;
+        nameArray.push(bookName);
+        authorArray.push(authorName);
+        typeArray.push(bookType);
+    
+        localStorage.setItem("nameText", JSON.stringify(nameArray));
+        localStorage.setItem("authorText", JSON.stringify(authorArray));
+        localStorage.setItem("typeText", JSON.stringify(typeArray));
+    
+    
+    };
+    validate(book) {
+        let libraryFrom = document.getElementById("libraryFrom");
+        if (book.name.length < 2 || book.author.length < 2 || book.type.length < 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    
+    };
+    showMassage(type, massage, strongMassage) {
+
+        let alertMassage = document.getElementById("alert");
+        alertMassage.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+        <strong>${strongMassage}</strong> ${massage}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`
+        setTimeout(function () {
+            alertMassage.innerHTML = "";
+        }, 2000)
+    };
+    displayBooks() {
+
+        let nameText = localStorage.getItem("nameText");
+        let authorText = localStorage.getItem("authorText");
+        let typeText = localStorage.getItem("typeText");
+        if (nameText == null) {
+            var nameArray = [];
+            var authorArray = [];
+            var typeArray = [];
+        }
+        else {
+            var nameArray = JSON.parse(nameText);
+            var authorArray = JSON.parse(authorText);
+            var typeArray = JSON.parse(typeText);
+        }
+    
+        if (nameText == "[]" || localStorage.length == 0) {
+            document.getElementById("notBook").style.display = "block";
+        }
+        else {
+            document.getElementById("notBook").style.display = "none";
+        }
+        let uiString = "";
+        let tableBody = document.getElementById("tableBody");
+        nameArray.forEach(function (element, index) {
+            uiString += `
+                
+                    <tr class="tableRow">
+                        <th scope="row">${index + 1}</th>
+                        <td class="bookNameClass">${element}</td>
+                        <td class="bookAuthorClass">${authorArray[index]}</td>
+                        <td class="bookTypeClass">${typeArray[index]}</td>
+                        <td><button  class="btn btn-light" onclick="display.deleteBook(${index})">Delete BooK</button></td>
+                    </tr>
+    
+        `
+    
+        });
+    
+        tableBody.innerHTML = uiString;
+    
+    
+    };
+    deleteBook(index) {
+        let nameText = localStorage.getItem("nameText");
+        let authorText = localStorage.getItem("authorText");
+        let typeText = localStorage.getItem("typeText");
+        if (nameText == null) {
+            nameArray = [];
+            authorArray = [];
+            typeArray = [];
+    
+        }
+        else {
+            nameArray = JSON.parse(nameText);
+            authorArray = JSON.parse(authorText);
+            typeArray = JSON.parse(typeText);
+        }
+        nameArray.splice(index, 1);
+        authorArray.splice(index, 1);
+        typeArray.splice(index, 1);
+        localStorage.setItem("nameText", JSON.stringify(nameArray));
+        localStorage.setItem("authorText", JSON.stringify(authorArray));
+        localStorage.setItem("typeText", JSON.stringify(typeArray));
+        displayBooks();
+    };
+    
+};
+
+document.getElementById("searchTxt").addEventListener("input", function search(e) {
+    inputValue = document.getElementById("searchTxt").value.toLowerCase();
+    rows=document.getElementsByClassName("tableRow");
+    let i=0;
+    Array.from(rows).forEach(function(element){
+        let bookName=element.getElementsByClassName("bookNameClass")[0].innerText.toLowerCase();
+        let bookAuthor=element.getElementsByClassName("bookAuthorClass")[0].innerText.toLowerCase();
+        let bookType=element.getElementsByClassName("bookTypeClass")[0].innerText.toLowerCase();
+        
+        console.log(bookAuthor)
+        console.log(bookName);
+        if (bookName.includes(inputValue) || bookType.includes(inputValue) || bookAuthor.includes(inputValue)){
+            
+            element.style.display="table-row";
+            i++;
+            document.getElementById("heads5").style.display="none";
+        }
+        else{
+            element.style.display="none";
+        }
+    })
+    if (i==0){
+        document.getElementById("heads5").style.display="block";
+    }
+
+
+});
+let libraryFrom = document.getElementById("submitBtn");
+function libraryFromSubmit(e) {
+    let name = document.getElementById("bookName").value;
+    let author = document.getElementById("authorName").value;
+
+    let python = document.getElementById("python");
+    let c = document.getElementById("c");
+    let javascript = document.getElementById("javascript");
+    let type;
+    if (python.checked) {
+        type = python.value;
+    }
+    else if (c.checked) {
+        type = c.value;
+    }
+    else {
+        type = javascript.value;
+    }
+
+
+    let book = new Book(name, author, type)
+    var display = new Display();
+
+    if (display.validate(book)) {
+        display.addToStorage(book);
+        display.displayBooks();
+
+        display.showMassage("success", " Added Book . ", "Succesfully")
+    }
+    else {
+        display.showMassage("danger", " Please Fulfill Valid Information.", "Error")
+    }
+    display.clear();
+    e.preventDefault();
+}
+libraryFrom.addEventListener("click", libraryFromSubmit);
+function deleteAll() {
+    localStorage.clear();
+    display.displayBooks();
+
+};
